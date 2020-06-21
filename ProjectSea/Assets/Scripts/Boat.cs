c20d74eff1c8c9c3528c4cebfbca79e4751dfa62
Pulpo <pol.gomar@enti.cat>
2020-06-21 13:30:34 +0200

    WIP Sea

---
+++ ProjectSea/Assets/Scripts/Boat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat : MonoBehaviour {
+    public Rigidbody RB;
+
+    private void Start() {
+        
+    }
+
+    private void FixedUpdate() {
+        RB.AddForce(new Vector3(0.5f, 0, 0));
+    }
+}
+
---
+++ ProjectSea/Assets/Scripts/Floater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour {
+
+    public Rigidbody RB;
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
+    public int floaterCount = 1;
+    public float waterDrag = 0.99f;
+    public float waterAngularDrag = 0.5f;
+
+    private void FixedUpdate() {
+        RB.AddForceAtPosition(Physics.gravity / floaterCount, transform.position, ForceMode.Acceleration);
+
+        float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x);
+        if (transform.position.y < waveHeight) {
+            float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+            RB.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementAmount, 0f), transform.position ,ForceMode.Acceleration);
+            RB.AddForce(displacementMultiplier * -RB.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            RB.AddTorque(displacementMultiplier * -RB.angularVelocity * waterAngularDrag* Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
