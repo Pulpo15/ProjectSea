ff4fc6715c312b5e49cb9aa830f073e6ce4df816
Pulpo <pol.gomar@enti.cat>
2020-08-28 14:08:36 +0200

    Boat Physics

---
+++ ProjectSea/Assets/zOld/Old/WaterManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class WaterManager : MonoBehaviour {
+
+    private MeshFilter meshFilter;
+    private float randomOffset;
+
+    private void Awake() {
+        meshFilter = GetComponent<MeshFilter>();
+    }
+
+    private void Start() {
+        randomOffset = Random.Range(1.1f, 5f);    
+    }
+
+    private void Update() {
+        Vector3[] vertices = meshFilter.mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++) {
+            vertices[i].y = WaveManager.instance.GetWaveHeight(transform.position.x + vertices[i].x, transform.position.z + vertices[i].z);
+        }
+        meshFilter.mesh.vertices = vertices ;
+        meshFilter.mesh.RecalculateNormals();
+    }
+}
