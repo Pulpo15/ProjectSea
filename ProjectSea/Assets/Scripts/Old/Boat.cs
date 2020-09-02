e61126733e671b44c5f93ab9fb50e4b2af5102f1
Pulpo <pol.gomar@enti.cat>
2020-09-02 20:47:42 +0200

    Revert "Boat Physics"
    This reverts commit ff4fc6715c312b5e49cb9aa830f073e6ce4df816.

---
+++ ProjectSea/Assets/Scripts/Old/Boat.cs
@@ -0,0 +1,46 @@
+﻿using PhysicsHelp;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat : MonoBehaviour {
+
+    [Header("MotorProperties")]
+    public Transform Motor;
+    public float steerPower = 500f;
+    public float power = 5f;
+    public float maxSpeed = 10f;
+    public float drag = 0.1f;
+
+    private Rigidbody RB;
+    private Quaternion StartRotation;
+
+    private void Awake() {
+        RB = GetComponent<Rigidbody>();
+        StartRotation = Motor.localRotation;
+    }
+
+    private void FixedUpdate() {
+        Vector3 forceDirection = transform.forward;
+        float steer = 0;
+
+        if (Input.GetKey(KeyCode.A))
+            steer = 1;
+        if (Input.GetKey(KeyCode.D))
+            steer = -1;
+
+        RB.AddForceAtPosition(steer * transform.right * steerPower / 100f, Motor.position);
+
+        Vector3 forward = Vector3.Scale(new Vector3(1, 0, 1), transform.forward);
+
+        if (Input.GetKey(KeyCode.W))
+            PhysicsHelper.ApplyForceToReachVelocity(RB, forward * maxSpeed, power);
+        if (Input.GetKey(KeyCode.S))
+            PhysicsHelper.ApplyForceToReachVelocity(RB, forward * -maxSpeed, power);
+
+
+        //transform.Translate(Vector3.forward + Vector3.right * 0.1f);
+        //RB.AddForce(transform.forward * Time.deltaTime * speed);
+    }
+}
+
--- ProjectSea/Assets/Scripts/Physics/BasicFloater.cs
+++ ProjectSea/Assets/Scripts/Physics/BasicFloater.cs
@@ -7,14 +7,14 @@ public class BasicFloater : MonoBehaviour {
     public float AirDrag;
     public float WaterDrag;
     public float AngularDrag;
+    public Rigidbody RB;
     public Transform[] FloatPoints;
 
     protected Waves Waves;
-    protected Rigidbody RB;
 
     private void Awake() {
         Waves = FindObjectOfType<Waves>();
-        RB = GetComponent<Rigidbody>();
+        //RB = GetComponent<Rigidbody>();
     }
 
     private void Start() {
@@ -22,10 +22,11 @@ public class BasicFloater : MonoBehaviour {
     }
 
     private void FixedUpdate() {
+        //RB.centerOfMass = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         for (int i = 0; i < FloatPoints.Length; i++) {
             if (FloatPoints[i].position.y > Waves.GetHeight(FloatPoints[i].position)) {
                 RB.drag = AirDrag;
-                RB.AddForceAtPosition(new Vector3(0f, Physics.gravity.y, 0), FloatPoints[i].position);
+                RB.AddForceAtPosition(new Vector3(0f, Physics.gravity.y,0), FloatPoints[i].position);
                 RB.AddTorque(RB.angularVelocity * WaterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
             } else {
                 RB.drag = WaterDrag;
@@ -36,7 +37,6 @@ public class BasicFloater : MonoBehaviour {
     }
 
     private void OnDrawGizmos() {
-        //Draw Center
         Gizmos.color = Color.red;
         Gizmos.DrawCube(transform.position, Vector3.one * 1.2f);
         if (FloatPoints == null)
@@ -45,7 +45,7 @@ public class BasicFloater : MonoBehaviour {
         for (int i = 0; i < FloatPoints.Length; i++) {
             if (FloatPoints[i] == null)
                 continue;
-            //Draw Floaters
+            //Draw Sphere
             Gizmos.color = Color.green;
             Gizmos.DrawSphere(FloatPoints[i].position, 0.1f);
         }
